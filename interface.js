d93cd658be19a915ffde27369ad11a0d995b5f31
annapikachu2005-cell <annapikachu2005@gmail.com>
2026-05-13 20:54:54 +0700

    Add files via upload

--- interface.js
+++ interface.js
@@ -1,10 +1,24 @@
-let log = console.log;
+//Необходимо разместить по центру весь дизайн.
 
+//Текст внутри кнопок должен оставаться на своем месте и не заезжать за пределы экрана или кнопок. При любом размере экрана пользователь должен иметь возможность нажать на кнопку без каких-либо препятствий.
+//Кнопка список /колонки должна иметь две функции. В зависимости от отображаемого на экране режима соответствующее слово должно выделяться ярким белым цветом.
+//Вариант с двумя панелями должен иметь яркую черту посередине как показано на рисунке.
+//Дополнительные информация в списке должна постоянно отображаться при активации варианта просмотра список.
+
+//Размер текста должен подстраиваться под размер экрана.
+//
+//
+//
+//
+
+
+
+let log = console.log;
 
 let arrow = document.getElementById("arrow");
 let list = document.getElementById("list");
 let icons = document.getElementById("icons");
-let pannels = document.getElementById("pannels");
+let displaySettings = document.getElementById("arrow");
 let footer = document.querySelector("footer");
 let tabs = document.getElementById("tabs");
 let main = document.querySelector("main");
@@ -16,24 +30,28 @@ let options = document.getElementsByClassName("options");
 footer.style.display = "none";
 header.style.display = "none"; 
 
-let newValues = ["reset selection","group description", "select description", "transfer media", "change srce/ dest", "display settings"];
+let newValues = ["","list colums", "card icons", "cropping", "gallery screen", "two panels"];
+//let newValues = ["reset selection","group description", "select description", "transfer media", "change srce/ dest", "display settings"];
 
 galleryScreen.addEventListener("click", function() {
    header.style.display = "block"; 
 
 });
-pannels.addEventListener("click", function(){
+
+displaySettings.addEventListener("click", function(){
     main.classList.add("double-pannel");
     main.classList.remove("card_pannel");
-            for (let i = 0; i < options.length; i++) {      
+    for (let i = 0; i < options.length; i++) {      
         bottom[i].style.display = "none"; 
-        }
+    }
 });
 
 list.addEventListener("click", function(){
     log("hello");
     main.classList.remove("double-pannel", "card_pannel");
-
+        for (let i = 0; i < options.length; i++) {        
+        options[i].innerHTML= '<div class="options"><img src="IMG_2353.jpeg" alt=""><span class="lables"><span class = "fileName">IMG_20260406s.jpg </span><span class = "bottom"><span>4:3</span> <a href="">1200:900 pixels 11,2 Mb</a><span>.jpg</span></span></span></div>';
+        }
 });
 
 tabs.addEventListener("click", function(){
@@ -47,7 +65,7 @@ icons.addEventListener("click", function(){
     main.classList.remove("double-pannel");
     main.classList.add("card_pannel");
     for (let i = 0; i < options.length; i++) {      
-        options[i].innerHTML = '<img src="IMG_2353.jpeg" alt=""><br><figcaption> <span class = "fileName">IMG_20260406s.jpg </span></figcaption>'; 
+        options[i].innerHTML = ' <img src="IMG_2353.jpeg" alt=""> <span class="lables"><span class = "fileName">IMG_20260406s.jpg </span></span>'; 
         }
 
 });
