b3ef08054c27863ee8d7340d2ce634db35ca3ee9
annapikachu2005-cell <annapikachu2005@gmail.com>
2026-06-22 21:17:40 +0700

    Add files via upload

--- interface.js
+++ interface.js
@@ -1,6 +1,6 @@
 
 
- /* 0. При первой загрузке страницы должно              отображаться общее меню.
+ /* 
 Кнопка список /колонки должна иметь две функции. В зависимости от отображаемого на экране режима соответствующее слово должно выделяться ярким белым цветом.
 Вариант с двумя панелями должен иметь яркую черту посередине как показано на рисунке.
 Дополнительные информация в списке должна постоянно отображаться при активации варианта просмотра список.
@@ -8,7 +8,8 @@
 Необходимо доделать крупные кнопки с названием папок.
 Кнопки с названием папок должны иметь разные цвета в зависимости от их активности или неактивности.
 Необходимо как можно ближе имитировать вид кнопок с названием папок.
-Кнопки с названием папок имеют три разных состояния в зависимости от выбранного режима просмотра.*/
+Кнопки с названием папок имеют три разных состояния в зависимости от выбранного режима просмотра.
+*/
 
 
 
@@ -17,15 +18,24 @@ let log = console.log;
 let resetSelection = document.getElementById("reset-selection");
 let groupDescription = document.getElementById("group-description");
 let selectDescription = document.getElementById("select-description");
+
+let displaySettings = document.getElementById("display-settings"); 
+
+let changeSrceDest = document.getElementById("changeSrceDest"); 
+
+
+
+log(changeSrceDest);
 let footer = document.querySelector("footer");
 let tabs = document.getElementsByClassName("tabs");
 let main = document.querySelector("main");
 let header = document.querySelector("header");
-let displaySettings = document.getElementById("display-settings"); 
+
 let bottom = document.getElementsByClassName("bottom");
 let options = document.getElementsByClassName("options");
 let elements = document.getElementsByClassName("lables");
 let images = document.querySelectorAll("img");
+
 let menuNavigaton = document.getElementById("menu-navigaton");
 
 let settingsOn = false;
@@ -41,7 +51,9 @@ log(toSmall);
 displaySettings.addEventListener("click", function(){
     let buttons = footer.querySelectorAll("button"); 
     //settingsOn = true
-    for (let i = 0; i <= newValues.length-1; i++) {
+    log(buttons[0]);
+    buttons[0].innerHTML = "<img src='arrow.svg'>"
+    for (let i = 1; i <= newValues.length-1; i++) {
         //log(newValues[i]);
         //log(buttons.textContent);
         log(buttons[i].textContent);
@@ -66,6 +78,7 @@ resetSelection.addEventListener("click", function(){
         log(buttons[i].textContent);
         buttons[i].textContent = oldValues[i];
     }
+    
 });
 settingsOn = false;
 /*displaySettings.addEventListener("click", function(){
@@ -80,17 +93,22 @@ settingsOn = false;
 
 groupDescription.addEventListener("click", function(){
     /*main.classList.value = "";*/
+    main.style = "";
         for (let i = 0; i <= options.length; i++) {        
         /*options[i].innerHTML= '<div class="options"><img src="IMG_2353.jpeg" alt=""><span class="lables"><span class = "fileName">IMG_20260406s.jpg </span><span class = "bottom"><span>4:3</span> <a href="">1200:900 pixels 11,2 Mb</a><span>.jpg</span></span></span></div>';*/
        bottom[i].style.display = "block";
         log(images[i]);
-        images[i].style.height="6vh"
-        images[i].style.width="7vh"
+        //images[i].style.height="5.5vh"
+        images[i].style.width="130%";
+        images[i].style.hieght="auto";
+        options[i].style="";
+        //options[i].style.gridTemplateColumns = "3rem 20rem";
         } 
           /*main.style.gridTemplateColumns = "0.75fr 0.75fr 0.75fr 0.75fr";*/
           //main.style.cssText = "";
-          options[i].style.display = "grid";
-          main.style.gridTemplateColumns = "3rem 20rem";
+          
+          //options[i].style.display = "grid";
+          //main.style.gridTemplateColumns = "3rem 20rem";
           
 
 
@@ -116,12 +134,31 @@ selectDescription.addEventListener("click", function(){
      //main.style.display = "grid";
     //main.style.gridTemplateColumns = "0.75fr 0.75fr 0.75fr 0.75fr";
     main.style.gridTemplateColumns = "0.5fr 0.5fr 0.5fr 0.5fr";
+    main.style.width = "60%"
     //main.style.display = "flex";
     //main.style.justifyContent = "space-between";
 
 
 });
 
+changeSrceDest.addEventListener("click", function(){
+    log("hello");
+    header.style.display = "block"; 
+    /*    height: 50vh;
+    background-color: rgb(62,63,65);
+    border-bottom: 10px solid #a9d6fc; */
+    header.style.hieght = "50vh";
+    header.style.backgroundColor = "rgb(62,63,65)";
+})
+
+displaySettings.addEventListener("click", function(){
+    log("hello");
+    main.style=""
+
+     main.style.gridTemplateColumns = "0.5fr 0.5fr"
+     main.style.width="90%"
+    
+});
 
 }
 tabs[0].addEventListener("click", function(){
