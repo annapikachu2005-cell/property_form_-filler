53c6da2653b6b070589d55430c66586a4a9a4e24
annapikachu2005-cell <annapikachu2005@gmail.com>
2026-07-06 21:06:30 +0700

    Add files via upload

--- interface.js
+++ interface.js
@@ -19,57 +19,69 @@ let header = document.querySelector("header");
 let bottom = document.getElementsByClassName("bottom");
 let options = document.getElementsByClassName("options");
 let elements = document.getElementsByClassName("lables");
-let images = document.querySelectorAll("img");
+
 let menuNavigaton = document.getElementById("menu-navigaton");
-//let buttons = footer.querySelectorAll("button"); 
-let buttons = document.querySelectorAll("button"); 
+let buttons = footer.querySelectorAll("button"); 
+//let buttons = document.querySelectorAll("button"); 
 let settingsOn = false;
 menuNavigaton.style.display = "none";
 header.style.height = "0"; 
-
+log(buttons);
+log(buttons[0].textContent);
 let newValues = ["","list colums", "card icons", "cropping", "gallery screen", "two panels"];
 let oldValues = ["reset selection","group description", "select description", "transfer media", "change srce/ dest", "display settings"];
-const toSmall = window.matchMedia("max-width: 480px");
+//const toSmall = window.matchMedia("max-width: 480px");
 //console.log(window.innerWidth);
-settingsOn = false;
+
 displaySettings.addEventListener("click", function(){
+    let images = document.querySelectorAll("img");
     //log(buttons[0]);
-    buttons[0].innerHTML = "<img src='arrow_icon.png'>"
-    for (let i = 1; i <= newValues.length; i++) {
-        log(buttons[i].textContent);
+   // buttons[0].innerHTML = "<img src='arrow_icon.png'>"
+    for (let i = 0; i <= newValues.length - 1; i++) {
+        //log(buttons[i].textContent);
         buttons[i].textContent = newValues[i];
     }
-    settingsOn = true;
+
+    resetSelection.addEventListener("click", function(){
+        let buttons = footer.querySelectorAll("button"); 
+        for (let i = 0; i <= newValues.length-1; i++) {
+            //log(buttons[i].textContent);
+            buttons[i].textContent = oldValues[i];
+        }
+}
+);
 });
-//settingsOn = true;
 
-if (settingsOn) {
-    
-log("in the event");
 
-resetSelection.addEventListener("click", function(){
-         let buttons = footer.querySelectorAll("button"); 
-    for (let i = 0; i <= newValues.length-1; i++) {
-        log(buttons[i].textContent);
-        buttons[i].textContent = oldValues[i];
-    }
-});
-//settingsOn = false;
-
-groupDescription.addEventListener("click", function(){
+    log("In in the event")
+    groupDescription.addEventListener("click", function(){
+    log("groupDescription");
     main.style = "";
+
         for (let i = 0; i <= options.length; i++) {  
             log(bottom[i]);      
        //bottom[i].style.display = "block";
         log(images[i]);
         images[i].style.width="130%";
         images[i].style.hieght="auto";
-        options[i].style="";
-        
+        options[i].style="";    
         } 
-});
 
-selectDescription.addEventListener("click", function(){      
+
+changeSrceDest.addEventListener("click", function(){
+    log("hello");
+    if (buttons[4] == ''){
+        log("in the event");
+    };
+    //header.style.display = "block"; 
+    /*
+    border-bottom: 10px solid #a9d6fc; */
+    log(header.style.height);
+    header.style.height= "50vh";
+    log(header.style.height);
+    header.style.backgroundColor = "rgb(62,63,65)";
+})
+ selectDescription.addEventListener("click", function(){      
     for (let i = 0; i < options.length; i++) {
         bottom[i].style.display = "none";
         images[i].style.width = "70%";
@@ -81,33 +93,28 @@ selectDescription.addEventListener("click", function(){
         elements[i].style.marginTop = "auto";
     }
     main.style.gridTemplateColumns = "0.5fr 0.5fr 0.5fr 0.5fr";
-    main.style.width = "60%"
-});
+    main.style.width = "60%"})
 
-changeSrceDest.addEventListener("click", function(){
-    log("hello");
-    //header.style.display = "block"; 
-    /*
-    border-bottom: 10px solid #a9d6fc; */
-    log(header.style.height);
-    header.style.height= "50vh";
-    log(header.style.height);
-    header.style.backgroundColor = "rgb(62,63,65)";
-})
+    })
 
-displaySettings.addEventListener("click", function(){
-    log("hello");
-    main.style=""
 
-     main.style.gridTemplateColumns = "0.25fr 0.25fr"
-     main.style.width="90%"
-     //main.style.borderRight = "solid rgba(49, 87, 255, 1) 3px"
-     //let line=document.createElement("div");
-     //line.style.backgroundColor=" rgba(169, 185, 255, 1) "
-     //main.append(line);
-    
-});
-}
+    //settingsOn = true;
+   
+ buttons[0].innerHTML = "<img src='arrow_icon.png'>"
+ //settingsOn = !settingsOn
+
+//settingsOn = true;
+
+//log(buttons[1].textContent);
+
+ //log(settingsOn);
+/*if (buttons[1].textContent == "list") {
+log("in the event");
+//settingsOn = false;
+}else{
+    log("fail");
+}*/
+
 tabs[0].addEventListener("click", function(){
     log("hello");
      menuNavigaton.style.display = "block";
