611f95156b13c771dbd3026699afa22302e22152
annapikachu2005-cell <annapikachu2005@gmail.com>
2026-07-08 20:46:36 +0700

    Add files via upload

--- interface.js
+++ interface.js
@@ -21,7 +21,7 @@ let options = document.getElementsByClassName("options");
 let elements = document.getElementsByClassName("lables");
 
 let menuNavigaton = document.getElementById("menu-navigaton");
-let buttons = footer.querySelectorAll("button"); 
+let buttons = document.querySelectorAll("button"); 
 //let buttons = document.querySelectorAll("button"); 
 let settingsOn = false;
 menuNavigaton.style.display = "none";
@@ -30,8 +30,7 @@ log(buttons);
 log(buttons[0].textContent);
 let newValues = ["","list colums", "card icons", "cropping", "gallery screen", "two panels"];
 let oldValues = ["reset selection","group description", "select description", "transfer media", "change srce/ dest", "display settings"];
-//const toSmall = window.matchMedia("max-width: 480px");
-//console.log(window.innerWidth);
+
 
 displaySettings.addEventListener("click", function(){
     let images = document.querySelectorAll("img");
@@ -41,82 +40,21 @@ displaySettings.addEventListener("click", function(){
         //log(buttons[i].textContent);
         buttons[i].textContent = newValues[i];
     }
-
-    resetSelection.addEventListener("click", function(){
-        let buttons = footer.querySelectorAll("button"); 
-        for (let i = 0; i <= newValues.length-1; i++) {
-            //log(buttons[i].textContent);
-            buttons[i].textContent = oldValues[i];
-        }
-}
-);
-});
-
-
-    log("In in the event")
-    groupDescription.addEventListener("click", function(){
-    log("groupDescription");
-    main.style = "";
-
-        for (let i = 0; i <= options.length; i++) {  
-            log(bottom[i]);      
-       //bottom[i].style.display = "block";
-        log(images[i]);
-        images[i].style.width="130%";
-        images[i].style.hieght="auto";
-        options[i].style="";    
-        } 
-
-
-changeSrceDest.addEventListener("click", function(){
-    log("hello");
-    if (buttons[4] == ''){
-        log("in the event");
-    };
-    //header.style.display = "block"; 
-    /*
-    border-bottom: 10px solid #a9d6fc; */
-    log(header.style.height);
-    header.style.height= "50vh";
-    log(header.style.height);
-    header.style.backgroundColor = "rgb(62,63,65)";
-})
- selectDescription.addEventListener("click", function(){      
-    for (let i = 0; i < options.length; i++) {
-        bottom[i].style.display = "none";
-        images[i].style.width = "70%";
-        images[i].style.height = "130%";
-        options[i].style.display = "flex";
-        options[i].style.justifyContent = "space-between";
-        options[i].style.flexDirection = "column"
-        elements[i].style.alignItems = "flex-end";
-        elements[i].style.marginTop = "auto";
-    }
-    main.style.gridTemplateColumns = "0.5fr 0.5fr 0.5fr 0.5fr";
-    main.style.width = "60%"})
-
     })
 
-
-    //settingsOn = true;
-   
- buttons[0].innerHTML = "<img src='arrow_icon.png'>"
- //settingsOn = !settingsOn
-
-//settingsOn = true;
-
-//log(buttons[1].textContent);
-
- //log(settingsOn);
-/*if (buttons[1].textContent == "list") {
-log("in the event");
-//settingsOn = false;
-}else{
-    log("fail");
-}*/
+for (let i = 0; i < buttons; i++) {
+    buttons[i].addEventListener("click", function(){
+        if (buttons[i].textContent.trim() === "list colums"){
+            console.log("list");
+        };
+        if (buttons[i].textContent.trim() === "select description"){
+            console.log("select description");
+        };
+    });
+}
 
 tabs[0].addEventListener("click", function(){
-    log("hello");
+    console.log("hello");
      menuNavigaton.style.display = "block";
     tabs[0].style.bottom = "10vh";
 });
