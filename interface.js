c8ce5e4e16e5b1b1b33814984b83c384eac0a05c
annapikachu2005-cell <annapikachu2005@gmail.com>
2026-07-13 21:20:49 +0700

    Add files via upload

--- interface.js
+++ interface.js
@@ -42,19 +42,29 @@ displaySettings.addEventListener("click", function(){
     })
 
 //buttons[0].addEventListener("click", function(){alert("OK!!")})
+
+
+
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(){
         if (buttons[i].textContent.trim() === "list colums"){
+
             console.log("list");
+            main.style.display = "grid";
+             for (let n = 0; n < options.length; n++) {
+             bottom[n].hidden = false;
+            images[n].style.height = "80%";
+            }
         };
         if (buttons[i].textContent.trim() === "card icons"){
             console.log("card icons");
                 for (let j = 0; j < options.length; j++) {
-        bottom[j].style.display = "none";
+        bottom[j].hidden = true;
         images[j].style.height = "130%";
         options[j].style.display = "flex";
         options[j].style.justifyContent = "space-between";
-        options[j].style.flexDirection = "column"
+        options[j].style.flexDirection = "column";
+        
     
     }
     main.style.gridTemplateColumns = "0.5fr 0.5fr 0.5fr 0.5fr";
