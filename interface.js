d8a03c8b3a3b571c7183e2cdf5c18cbb051ed28f
annapikachu2005-cell <annapikachu2005@gmail.com>
2026-07-16 21:27:05 +0700

    Add files via upload

--- interface.js
+++ interface.js
@@ -1,11 +1,3 @@
-/* 
-Кнопка список /колонки должна иметь две функции. В зависимости от отображаемого на экране режима соответствующее слово должно выделяться ярким белым цветом.
-Вариант с двумя панелями должен иметь яркую черту посередине как показано на рисунке.
-Информация в карточках должна быть аккуратно организована и корректно отображаться независимо от размера экрана.
-*/
-
-//const { createElement } = require("react");
-
 let log = console.log;
 let resetSelection = document.getElementById("reset-selection");
 let groupDescription = document.getElementById("group-description");
@@ -21,52 +13,42 @@ let options = document.getElementsByClassName("options");
 let elements = document.getElementsByClassName("lables");
 let menuNavigaton = document.getElementById("menu-navigaton");
 let buttons = document.querySelectorAll("button"); 
-let images = document.querySelectorAll("img");
-//let buttons = document.querySelectorAll("button"); 
-let settingsOn = false;
+let images = document.querySelectorAll("img"); 
 menuNavigaton.style.display = "none";
-header.style.height = "0"; 
-log(buttons);
-log(buttons[0].textContent);
+header.hidden = true; 
+
 let newValues = ["","list colums", "card icons", "cropping", "gallery screen", "two panels"];
 let oldValues = ["reset selection","group description", "select description", "transfer media", "change srce/ dest", "display settings"];
 
 displaySettings.addEventListener("click", function(){
     let images = document.querySelectorAll("img");
-    //log(buttons[0]);
-   // buttons[0].innerHTML = "<img src='arrow_icon.png'>"
     for (let i = 0; i <= newValues.length - 1; i++) {
-        //log(buttons[i].textContent);
         buttons[i].textContent = newValues[i];
     }
-    })
-
-//buttons[0].addEventListener("click", function(){alert("OK!!")})
-
-
+})
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(){
-        if (buttons[i].textContent.trim() === "list colums"){
-
+        if (buttons[i].textContent.trim() === "list colums"){         
             console.log("list");
             main.style.display = "grid";
              for (let n = 0; n < options.length; n++) {
              bottom[n].hidden = false;
             images[n].style.height = "80%";
+            options[n].style.display = "grid";
+             options[n].style.gridTemplateColumns = "4rem 15rem";
             }
+            main.style.gridTemplateColumns = "0.5fr" ;
         };
         if (buttons[i].textContent.trim() === "card icons"){
             console.log("card icons");
-                for (let j = 0; j < options.length; j++) {
-        bottom[j].hidden = true;
-        images[j].style.height = "130%";
-        options[j].style.display = "flex";
-        options[j].style.justifyContent = "space-between";
-        options[j].style.flexDirection = "column";
-        
-    
-    }
+                for (let j = 0; j < options.length; j++) {             
+                    bottom[j].hidden = true;
+                    images[j].style.height = "130%";
+                    options[j].style.display = "flex";
+                    options[j].style.justifyContent = "space-between";
+                    options[j].style.flexDirection = "column";        
+                }
     main.style.gridTemplateColumns = "0.5fr 0.5fr 0.5fr 0.5fr";
     main.style.width = "60%"
         };
@@ -75,22 +57,25 @@ for (let i = 0; i < buttons.length; i++) {
         };
         if (buttons[i].textContent.trim() === "gallery screen"){
             console.log("gallery screen");
+            header.hidden = false; 
         };
         if (buttons[i].textContent.trim() === "two panels"){
             console.log("two panels");
+           main.style.gridTemplateColumns = "0.5fr 0.5fr";
+           main.style.borderRight = "4px solid rgb(64, 64, 255)"
         };
     }
     )};
 
-
 tabs[0].addEventListener("click", function(){
     console.log("hello");
-     menuNavigaton.style.display = "block";
+    header.hidden = false; 
     tabs[0].style.bottom = "10vh";
 });
 
 tabs[1].addEventListener("click", function(){
     log("hello");
      menuNavigaton.style.display = "block";
+     menuNavigaton.hidden = false;
     tabs[0].style.bottom = "10vh";
 });
