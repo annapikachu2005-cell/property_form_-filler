09ca34c47473c0cc92b2a709679f105329bd7c27
annapikachu2005-cell <annapikachu2005@gmail.com>
2026-07-29 21:18:19 +0700

    Add files via upload

--- interface.js
+++ interface.js
@@ -1,11 +1,7 @@
 
-//do buttons one by one
 let log = console.log;
-let resetSelection = document.getElementById("reset-selection");
-let groupDescription = document.getElementById("group-description");
-let selectDescription = document.getElementById("select-description");
+
 let displaySettings = document.getElementById("display-settings"); 
-let changeSrceDest = document.getElementById("changeSrceDest"); 
 
 let footer = document.querySelector("footer");
 let tabs = document.getElementsByClassName("tabs");
@@ -13,17 +9,14 @@ let main = document.querySelector("main");
 let header = document.querySelector("header");
 
 let bottom = document.getElementsByClassName("bottom");
-let options = document.getElementsByClassName("options");
-let elements = document.getElementsByClassName("lables");
 let menuNavigaton = document.getElementById("menu-navigaton");
 let buttons = document.querySelectorAll("button"); 
-let images = document.querySelectorAll("img"); 
-menuNavigaton.style.display = "none";
-header.hidden = true; 
-let pannelsOn = false;
+
+menuNavigaton.hidden, header.hidden = true;
 let newValues = ["","list colums", "card icons", "cropping", "gallery screen", "two panels"];
 let oldValues = ["reset selection","group description", "select description", "transfer media", "change srce/ dest", "display settings"];
-let cardInfo = false;
+
+let pannelsOn, cardInfo, twoColumns = false;
 displaySettings.addEventListener("click", function(){
     for (let i = 0; i <= newValues.length - 1; i++) {
         buttons[i].textContent = newValues[i];
@@ -32,37 +25,42 @@ displaySettings.addEventListener("click", function(){
 
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function(){
-        if (buttons[i].textContent.trim() === "list colums"){         
-            console.log("list");
+        if (buttons[i].textContent.trim() === ""){  
+                for (let i = 0; i <= newValues.length - 1; i++) {
+        buttons[i].textContent = oldValues[i];
+    }
+        };
+        if (buttons[i].textContent.trim() === "list colums"){    
+            twoColumns = !twoColumns;     
             main.style.display = "grid";
-             for (let n = 0; n < options.length; n++) {
-             bottom[n].hidden = false;
-            options[n].style.display = "grid";
-             options[n].style.gridTemplateColumns = "3.6rem 15rem";
+             for (let n = 0; n < main.children.length; n++) {
+             bottom[n].hidden = true;
+             main.children[n].style.display = "grid";
+             main.children.style.gridTemplateColumns = "3.6rem 15rem";
+            main.children[n].children[0].style = "90%";
             }
-            main.style.gridTemplateColumns = "0.45fr 0.45fr" ;
-            
+            if(twoColumns){
+            main.style.gridTemplateColumns = "0.35fr 0.35fr" ;
+        }else{main.style.gridTemplateColumns = "0.35fr" ;}
         };
         if (buttons[i].textContent.trim() === "card icons"){
              cardInfo = !cardInfo; 
-            console.log("card icons");
-            for (let j = 0; j < options.length; j++) {             
+            for (let j = 0; j < main.childrenh; j++) {             
                     bottom[j].hidden = true;
-                    images[j].style.height = "130%";
-                    options[j].style.display = "flex";
-                    options[j].style.justifyContent = "space-between";
-                    options[j].style.flexDirection = "column";                         
+                    main.children[j].style.display = "flex";
+                    main.children[j].style.justifyContent = "space-between";
+                    main.children[j].style.flexDirection = "column";      
+                    main.children[j].children[0].style = "130%";                   
                 }
             if(cardInfo){
-                for (let j = 0; j < options.length; j++) {             
-                    bottom[j].hidden = false;
-                    
-                   
-                    
+                for (let j = 0; j <main.children .length; j++) {             
+                    bottom[j].hidden = true;                  
                 }}else{
-                for (let j = 0; j < options.length; j++) {             
-                    bottom[j].hidden = true;                      
-                }}
+                for (let j = 0; j < main.children.length; j++) {             
+                    bottom[j].hidden = false;                      
+                }
+            main.style.gridTemplateColumns = "0.35fr 0.35fr" ;
+            }
 
     main.style.gridTemplateColumns = "0.42fr 0.42fr 0.42fr 0.42fr";
 
@@ -75,7 +73,6 @@ for (let i = 0; i < buttons.length; i++) {
             header.hidden = false; 
         };
         if (buttons[i].textContent.trim() === "two panels"){
-            console.log("two panels");
            main.style.gridTemplateColumns = "0.45fr 0.45fr";
            main.style.borderRight = "6px solid rgb(73, 73, 255)";
  
@@ -83,15 +80,7 @@ for (let i = 0; i < buttons.length; i++) {
     }
     )};
 
-tabs[0].addEventListener("click", function(){
-    console.log("hello");
-    header.hidden = false; 
-    tabs[0].style.bottom = "10vh";
-});
-
-tabs[1].addEventListener("click", function(){
-    log("hello");
-     menuNavigaton.style.display = "block";
-     menuNavigaton.hidden = false;
-    tabs[0].style.bottom = "10vh";
-});
+for (let i= 0; i < tabs.length; i++) {
+    menuNavigaton.hidden = false;
+    tabs[0].style.bottom = "10vh";    
+}
